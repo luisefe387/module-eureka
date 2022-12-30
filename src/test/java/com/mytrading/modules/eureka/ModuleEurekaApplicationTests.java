ec4ddece8d5c96c11a714c68ce7c0cc5ae8a838e
Luis Fernando Pedroza Taborda <luisefe387@hotmail.es>
2022-12-30 17:07:28 -0500

    Carga de todo el proyecto inicial

---
+++ src/test/java/com/mytrading/modules/eureka/ModuleEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytrading.modules.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
